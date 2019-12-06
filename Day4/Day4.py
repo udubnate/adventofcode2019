d280e700d1e0d7406963bd9cee9df9a7aae8c943
Nate Bruneau <natebruneau@gmail.com>
2019-12-05 22:29:23 -0800

    completed never decrease check, just need to wrap it up, going to bed for now

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -6,6 +6,7 @@ def parse(numrange):
     for i in range(int(r[0]),int(r[1])+1):
         valid = True
         adjacent = False
+        neverdecrease = True
         for c in range(len(str(i))):
             fullstring = str(i)
             #adjacent check
@@ -13,8 +14,14 @@ def parse(numrange):
                 continue
             elif fullstring[c] == fullstring[c+1]:
                 adjacent = True
-                break
+            if int(fullstring[c]) <= int(fullstring[c+1]):
+                neverdecrease = True
+            else:
+                neverdecrease = False
+
             print("debug: current: " + str(fullstring[c]) + " next: " + str(fullstring[c + 1]))
         print(str(i) + " adjacent check : " + str(adjacent))
+        print(str(i) + " neverdecrease check : " + str(neverdecrease))
+
 
 parse(numrange)
