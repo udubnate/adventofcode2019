b6db694454d71884fb6380397ce63c6b2aba531d
Nate Bruneau <natebruneau@gmail.com>
2019-12-08 12:41:23 -0800

    Refactored after looking at much better code

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -1,6 +1,6 @@
 import logging
 logger = logging.getLogger()
-logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 numrange = "236491-713787"
 
@@ -8,27 +8,30 @@ def parse(numrange):
     count = 0
     r = numrange.split('-')
     for i in range(int(r[0]),int(r[1])+1):
-        valid = True
-        adjacent = False
-        neverdecrease = True
-        for c in range(len(str(i))):
-            fullstring = str(i)
-            #adjacent check
-            if c == len(fullstring)-1:
-                continue
-            elif fullstring[c] == fullstring[c+1]:
-                adjacent = True
-            if int(fullstring[c]) <= int(fullstring[c+1]):
-                pass
-            else:
-                neverdecrease = False
-
-            logger.debug("debug: current: " + str(fullstring[c]) + " next: " + str(fullstring[c + 1]))
-        logger.debug(str(i) + " adjacent check : " + str(adjacent))
-        logger.debug(str(i) + " neverdecrease check : " + str(neverdecrease))
-        if adjacent and neverdecrease:
+        adjacent = hasAdjacent(i)
+        decreasecheck = neverDecrease(i)
+        if adjacent and decreasecheck:
             count = count + 1
     return count
+
+def hasAdjacent(input):
+    adjacent = False
+    inputstr = str(input)
+    for i in range(len(inputstr)-1):
+         if inputstr[i] == inputstr[i+1]:
+            return True
+    return False
+
+
+def neverDecrease(input):
+    adjacent = False
+    inputstr = str(input)
+    for i in range(len(inputstr)-1):
+         if int(inputstr[i]) <= int(inputstr[i+1]):
+            pass
+         else:
+             return False
+    return True
 
 count = parse(numrange)
 print("Total matches: " + str(count))
